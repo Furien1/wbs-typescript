95178f765fdd60e7fe82db4a1a9ca31696bdcc01
Furien1 <laszlo.csonka.barca@gmail.com>
2022-01-31 11:40:04 +0100

    TypeScript introduction
    - how to install TypeScript
    -

---
+++ index.ts
@@ -0,0 +1,20 @@
+/*
+JS - JIT Compilation - Just in time 
+
+// Environment setup
+
+- TypeScript compiler is necessary to our Web Browser
+- it compiles the source code to JavaScript
+npm install ts-node
+
+*/
+
+
+// Example of Type Usage
+var firstName: string = 'Anna';
+console.log(firstName);
+
+// We need to compile to plain JS -> terminal, use TS compiler
+// tsc index.ts
+// run with "node index.js"
+// OR instead of two commands, we can use simply ts-node index.ts
